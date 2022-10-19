5b209a3a6391fa60bed07ff4823e30b17075dbf5
Maryna Kidun <marykidun@gmail.com>
2022-10-20 00:00:06 +0300

    Completed three tasks

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,18 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
+    static void divide(int toBeDivided) {
+        int divider = 5;
+        int result = toBeDivided / divider;
+        System.out.println("The result of division is " + result);
     }
+
+    public static void main(String[] args) {
+        divide(10);
+    }
+
+    {
+
+    }
+
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,13 @@
 package lang.print.gaps.task5;
+import java.util.*;
 
 public class TimeConvertor {
-    public void convert(float minutes) {
+    public static void convert(float minutes) {
+        float seconds = minutes * 60;
+
+        System.out.println("There are " + seconds + " seconds in " + minutes + " minutes");
+    }
+    public static void main(String[] args) {
+        convert(10);
     }
 }
